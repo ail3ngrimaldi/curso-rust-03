d8ea3d8850e8bcc388beeb9c8ea513838667040c
ail3ngrimaldi <ailenrgrimaldi@gmail.com>
2023-04-17 17:52:56 -0300

    finished lifetimes2

--- tests/lifetimes2.rs
+++ tests/lifetimes2.rs
@@ -5,8 +5,6 @@
 // we need to change?
 
 
-// I AM NOT DONE
-
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -17,15 +15,14 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 
 #[cfg(test)]
 mod tests {
-    use crate::longest;
     #[test]
     fn test_lifetime() {
         let string1 = String::from("long string is long");
         let result;
         {
             let string2 = String::from("xyz");
-            result = longest(string1.as_str(), string2.as_str()) //borrowed value doesnot live long enough
-        } //string2 dropped here while still borrowed
-        println!("The longest string is {}", result); //borrow later used here on result
+            result = super::longest(string1.as_str(), string2.as_str());
+            println!("The longest string is {}", result); 
+        }
     }
 }
